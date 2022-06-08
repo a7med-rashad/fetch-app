d72202afa8c4f81d0ba269233923fa8312cf45ff
a7med-rashad <a7medra4aad3@gmail.com>
2022-06-09 01:27:54 +0200

    finish

---
+++ js/main.js
@@ -0,0 +1,49 @@
+
+let theInput = document.querySelector(".get-link input")
+let getButton = document.querySelector(".get-button")
+let myData = document.querySelector(".show-data")
+
+
+getButton.onclick = function () {
+    getLink()
+}
+
+function getLink() {
+    if (theInput.value == "") {
+        myData.innerHTML = "<span>Please write the your username !</span>"
+    }else {
+        fetch(`https://api.github.com/users/${theInput.value}/repos`)
+        .then((link) => link.json())
+
+        .then((data) => {
+            myData.innerHTML = "";
+
+            data.forEach(link => {
+                let mainDiv = document.createElement("div")
+
+                mainDiv.appendChild(document.createTextNode(link.name))
+
+                mainDiv.className = "link-box"
+
+                let theUrl = document.createElement("a")
+
+                theUrl.appendChild(document.createTextNode("Visit"))
+
+                theUrl.href = `https://github.com/${theInput.value}/${link.name}`;
+
+                theUrl.setAttribute("target", "_blank");
+
+                mainDiv.appendChild(theUrl)
+
+                let starsText = document.createElement("span")
+
+                starsText.appendChild(document.createTextNode(`Stars  ${link.stargazers_count}`))
+
+                mainDiv.appendChild(starsText)
+
+                myData.appendChild(mainDiv)
+            });
+        });
+        
+    }
+}
